3e30cfe5ad852a9c0bedb60e668df24bf34adc51
Rahul Jha <rjha5516@gmail.com>
2025-08-15 19:39:32 +0530

    updated code

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,11 +4,13 @@ import FeaturedDestination from '../components/FeaturedDestination'
 import ExclusiveOffers from '../components/ExclusiveOffers'
 import Testinomial from '../components/Testinomial'
 import NewsLetter from '../components/NewsLetter'
+import RecommendedHotels from '../components/RecommendedHotels'
 
 const Home = () => {
   return (
     <>
       <Hero/>
+      <RecommendedHotels/>
       <FeaturedDestination/>
       <ExclusiveOffers/>
       <Testinomial />
