16b1dc56fae6d76b937639fa15c3ce82d5dea0bf
Rahul Jha <rjha5516@gmail.com>
2025-08-15 21:05:32 +0530

    finalized code

--- client/src/pages/MyBookings.jsx
+++ client/src/pages/MyBookings.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import Title from '../components/Title'
 import { assets, userBookingsDummyData } from '../assets/assets'
 import { useAppContext } from '../context/AppContext'
+import toast from 'react-hot-toast'
 
 const MyBookings = () => {
 
@@ -20,6 +21,22 @@ const MyBookings = () => {
             toast.error(error.message)
         }
     }
+
+    const handlePayment = async (bookingId) => {
+        try {
+            const {data} = await axios.post('/api/bookings/stripe-payment', {bookingId}, {headers: {Authorization: `Bearer ${await getToken()}`}})
+            
+            if (data.success) {
+                window.location.href = data.url
+            } else {
+                toast.error(data.message)
+            }
+        } catch (error) {
+            toast(error.message)
+        }
+    }
+
+
     useEffect(()=>{
         if (user) {
             fetchUserBookings()
@@ -81,7 +98,7 @@ const MyBookings = () => {
                             </p>
                         </div>
                         {!booking.isPaid && (
-                            <button className='px-4 py-1.5 mt-4 text-xs border border-gray-400 rounded-full hover:bg-gray-50 transition-all cursor-pointer'>
+                            <button onClick={()=> handlePayment(booking._id)} className='px-4 py-1.5 mt-4 text-xs border border-gray-400 rounded-full hover:bg-gray-50 transition-all cursor-pointer'>
                                 Pay Now
                             </button>
                         )}
